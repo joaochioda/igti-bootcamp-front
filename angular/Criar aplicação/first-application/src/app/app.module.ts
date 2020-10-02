06e4e63f8f8128ad8ddd7dea32d9caf62eddf183
joaochioda <joao.chioda@dextra-sw.com>
2020-10-02 19:05:44 -0300

    angular

---
+++ angular/Criar aplicação/first-application/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { C1Component } from './c1/c1.component';
+import { CounterComponent } from './counter/counter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    C1Component,
+    CounterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
