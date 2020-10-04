81d37c48a55d200aea350575f77000a87de60ad4
joaochioda <joao.chioda@dextra-sw.com>
2020-10-03 21:01:02 -0300

    css

--- angular/Criar aplicação/first-application/src/app/rating/rating.component.ts
+++ angular/Criar aplicação/first-application/src/app/rating/rating.component.ts
@@ -9,6 +9,9 @@ export class RatingComponent {
   @Input() rating: number;
   @Output() ratingChange = new EventEmitter<number>();
 
+  className: string = "box1";
+  boxSize: number = 80;
+
   onClick(i: number) {
     this.ratingChange.emit(i);
   }
