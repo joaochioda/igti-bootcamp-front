b01bef39bed400d00db3560fb692d98fb4a248ba
joaochioda <joao.chioda@dextra-sw.com>
2020-11-01 16:20:16 -0300

    forms

--- vue/petshop/src/components/Recepcao.vue
+++ vue/petshop/src/components/Recepcao.vue
@@ -1,68 +1,94 @@
 <template>
-	<div>
-		<h2>Bem vindo ao Petshop</h2>
-		<form>
-			<div>
-				<label class="espaco-lado">Nome do cliente:</label>
-				<input type="text" />
-			</div>
-			<div class="espaco-abaixo"></div>
-			<div>
-				<label class="espaco-lado">Raça do PET</label>
-				<select>
-					<option v-for="(raca, index) in racas" :key="index">
-						{{ raca.nome }}
-					</option>
-				</select>
-			</div>
-			<div class="espaco-abaixo"></div>
-			<div>
-				<label class="espaco-lado">Idade:</label>
-				<input type="number" />
-			</div>
-			<div class="espaco-abaixo"></div>
-			<div>
-				<label class="espaco-lado">Seviço</label>
-				<select>
-					<option v-for="(servico,index) in servicos" :key="index">
-						{{servico.nome}} {{servico.preco | grana}}
-					</option>
-				</select>
-			</div>
-			<div class="espaco-abaixo"></div>
-			<div>
-				<button>Cadastrar cliente</button>
-			</div>
-		</form>
-	</div>
+  <div>
+    <h2>Bem vindo ao Petshop</h2>
+    <form @submit.prevent="cadastrarCliente">
+      <div>
+        <label class="espaco-lado">Nome do cliente:</label>
+        <input type="text" v-model="cliente.nome" />
+      </div>
+      <div class="espaco-abaixo"></div>
+      <div>
+        <label class="espaco-lado">Raça do PET</label>
+        <select v-model="cliente.raca">
+          <option v-for="(raca, index) in racas" :key="index">
+            {{ raca.nome }}
+          </option>
+        </select>
+      </div>
+      <div class="espaco-abaixo"></div>
+      <div>
+        <label class="espaco-lado">Idade:</label>
+        <input type="number" v-model="cliente.idade" />
+        <div class="erro" v-if="cliente.idade > 30">
+          Tem certeza que é um cachorro ?
+        </div>
+      </div>
+
+      <div class="espaco-abaixo"></div>
+      <div>
+        <label class="espaco-lado">Seviço</label>
+        <select v-model="cliente.servico">
+          <option
+            v-for="(servico, index) in servicos"
+            :key="index"
+            :value="servico"
+          >
+            {{ servico.nome }} {{ servico.preco | grana }}
+          </option>
+        </select>
+      </div>
+      <div class="espaco-abaixo"></div>
+      <div>
+        <button
+          type="submit"
+          v-bind:disabled="
+            cliente.idade >= 30 ||
+              cliente.nome.length === 0 ||
+              cliente.raca.length === 0 ||
+              cliente.idade === 0 ||
+              cliente.servico.tipo.length === 0
+          "
+        >
+          Cadastrar cliente
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+import Cachorro from "@/models/cachorro";
 export default {
-	data() {
-		return {
-			racas: [],
-			servicos: []
-		};
-	},
-	async created() {
-		this.racas = await this.buscarRacas();
-		this.servicos = await this.buscarServicos();
-	},
-	methods: {
-		async buscarRacas() {
-			const { data } = await axios.get("http://localhost:3000/racas");
-			return data;
-		},
-		async buscarServicos() {
-			const { data } = await axios.get("http://localhost:3000/servicos");
-			return data;
-		},
-	}
+  data() {
+    return {
+      cliente: new Cachorro(),
+      racas: [],
+      servicos: []
+    };
+  },
+  async created() {
+    this.racas = await this.buscarRacas();
+    this.servicos = await this.buscarServicos();
+  },
+  methods: {
+    async buscarRacas() {
+      const { data } = await axios.get("http://localhost:3000/racas");
+      return data;
+    },
+    async buscarServicos() {
+      const { data } = await axios.get("http://localhost:3000/servicos");
+      return data;
+    },
+    cadastrarCliente() {
+      this.$emit("novo-cadastro", this.cliente);
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-
+.erro {
+  color: red;
+}
 </style>
--- vue/petshop/src/views/Petshop.vue
+++ vue/petshop/src/views/Petshop.vue
@@ -1,18 +1,18 @@
 <template>
-	<div>
-		<div class="row">
-			<div class="col">
-				<cadastro-cliente @novo-cadastro="novoCadastro"></cadastro-cliente>
-			</div>
-			<div class="col">
-				<Veterinario :clientes="clientesVeterinario"></Veterinario>
-				<Banho :clientes="clientesBanho"></Banho>
-			</div>
-		</div>
-		<div class="espaco-abaixo"></div>
-		<div class="espaco-abaixo"></div>
-		<Balanco></Balanco>
-	</div>
+  <div>
+    <div class="row">
+      <div class="col">
+        <cadastro-cliente @novo-cadastro="encaminharCliente"></cadastro-cliente>
+      </div>
+      <div class="col">
+        <Veterinario :clientes="clientesVeterinario"></Veterinario>
+        <Banho :clientes="clientesBanho"></Banho>
+      </div>
+    </div>
+    <div class="espaco-abaixo"></div>
+    <div class="espaco-abaixo"></div>
+    <Balanco></Balanco>
+  </div>
 </template>
 
 <script>
@@ -22,21 +22,21 @@ import Banho from "@/components/Banho.vue";
 import Balanco from "@/components/Balanco.vue";
 
 export default {
-	components: {
-		"cadastro-cliente": Recepcao,
-		Veterinario,
-		Banho,
-		Balanco
-	},
-	data() {
-		return {
-			clientesVeterinario: [],
-			clientesBanho: []
-		};
+  components: {
+    "cadastro-cliente": Recepcao,
+    Veterinario,
+    Banho,
+    Balanco
+  },
+  data() {
+    return {
+      clientesVeterinario: [],
+      clientesBanho: []
+    };
   },
   methods: {
-    novoCadastro(cliente) {
-      cliente.servico.tipo === 'banho'
+    encaminharCliente(cliente) {
+      cliente.servico.tipo === "banho"
         ? this.clientesBanho.push(cliente)
         : this.clientesVeterinario.push(cliente);
     }
